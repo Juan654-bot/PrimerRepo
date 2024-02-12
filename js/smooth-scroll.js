fa98b30e7a09644c304c85fedd024ed2a1c86fb1
Juan <juanmartinmaidana74@gmail.com>
2024-02-12 11:41:41 -0300

    agruegando el menutoggle deslizable parte1

--- js/smooth-scroll.js
+++ js/smooth-scroll.js
@@ -129,6 +129,10 @@ function isFooterVisible() {
 var footerShown = false;
 
 function toggleMenu() {
-    var menu = document.getElementById('menu-list');
-    menu.classList.toggle('show-menu');
+    var menu = document.getElementById('menu-slideout');
+    if (menu.style.width === '100%') {
+        menu.style.width = '0';
+    } else {
+        menu.style.width = '100%';
+    }
 }
